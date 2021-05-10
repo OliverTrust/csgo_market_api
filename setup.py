276ab74056c063c797bb1e66e55dbf869927df6c
Ins <letsfunbox@gmail.com>
2021-05-10 22:07:12 +0300

    fix

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
+install_requires = [
+    'requests'
+]
+
 setup(name='csgo_market_api',
-      version='0.1',
+      version='0.1.1',
       description='A simple python module for interacting with various parts of market.csgo.com',
       packages=['csgo_market_api'],
       author_email='letsfunbox@gmail.com',
+      install_requires=install_requires,
       zip_safe=False)
